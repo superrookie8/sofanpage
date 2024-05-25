39c5c33b6c741dc824ddf6ef42b56059a0edd91e
superrookie8 <74133113+superrookie8@users.noreply.github.com>
2024-05-25 18:23:48 +0900

    all

---
+++ src/app/admin/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProfileForm from "@/components/admin/ProfileForm";
+
+const ManageProfiles: React.FC = () => {
+	return (
+		<div>
+			<h1>Manage Profiles</h1>
+			<ProfileForm />
+		</div>
+	);
+};
+
+export default ManageProfiles;
---
+++ src/components/stats.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const Stats: React.FC<Props> = (props) => {
+	return <div></div>;
+};
+
+export default Stats;
