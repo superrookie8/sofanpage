93f239ab5d7df3e232338a7de5e8e22895a29c7a
superrookie8 <74133113+superrookie8@users.noreply.github.com>
2024-06-19 19:19:16 +0900

    user page get photos

--- src/components/photos.tsx
+++ src/components/photos.tsx
@@ -1,15 +1,43 @@
-import React from "react";
-import Carousel from "./carousel";
-import images from "./images";
+import React, { useEffect, useState } from "react";
+import ImageSlider from "./ImageSlider";
 
-interface Props {}
+interface Photo {
+	_id: string;
+	filename: string;
+	base64: string;
+	url: string;
+}
 
-const Photos: React.FC<Props> = (props) => {
+const GetPhotos: React.FC = () => {
+	const [photos, setPhotos] = useState<Photo[]>([]);
+
+	useEffect(() => {
+		const fetchPhotos = async () => {
+			try {
+				const response = await fetch("/api/getphotos", {
+					method: "GET",
+					cache: "no-store",
+				});
+				const data = await response.json();
+				if (response.ok) {
+					setPhotos(data.admin_photos);
+
+					console.log("data", data);
+				} else {
+					console.error("Failed to fetch photos", data);
+				}
+			} catch (error) {
+				console.error("Error fetching photos:", error);
+			}
+		};
+		fetchPhotos();
+	}, []);
+
+	const imageUrls = photos.map((photo) => photo.base64);
+
 	return (
-		<div>
-			<Carousel images={images} />
-		</div>
+		<div>{imageUrls.length > 0 && <ImageSlider images={imageUrls} />}</div>
 	);
 };
 
-export default Photos;
+export default GetPhotos;
