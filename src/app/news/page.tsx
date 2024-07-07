631028be58092aad18b3e455b29f672de879a0b5
superrookie8 <74133113+superrookie8@users.noreply.github.com>
2024-07-08 02:23:18 +0900

    unchange login

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -14,7 +14,6 @@ export default function News() {
 			<Header pathname="" />
 			<div className="flex justify-center items-center ">
 				<div className="bg-red-500 min-h-screen w-full flex justify-center p-8 relative">
-					<Sidebar />
 					<div className="bg-yellow-500 min-h-[580px] min-w-[1200px] flex p-8">
 						내용이 없어서?
 					</div>
