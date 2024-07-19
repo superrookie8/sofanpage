0fcb5e26910f42a9c3338d8be7b8c75854b33e87
superrookie8 <74133113+superrookie8@users.noreply.github.com>
2024-07-19 19:55:02 +0900

    카카오맵 재수정(비동기 코드 삭제)

--- src/components/kakaoMap.tsx
+++ src/components/kakaoMap.tsx
@@ -13,12 +13,6 @@ const Map = () => {
 
 	useEffect(() => {
 		const loadScript = () => {
-			if (window.kakao && window.kakao.maps) {
-				// 카카오 지도 API가 이미 로드된 경우 초기화
-				initializeMap();
-				return;
-			}
-
 			const script = document.createElement("script");
 			script.async = true;
 			script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false&libraries=services`;
@@ -26,53 +20,55 @@ const Map = () => {
 
 			script.onload = () => {
 				window.kakao.maps.load(() => {
-					initializeMap();
+					console.log("Kakao Maps is ready");
+					if (!selectedLocation) return;
+
+					const container = document.getElementById("map");
+					const options = {
+						center: new window.kakao.maps.LatLng(
+							selectedLocation.latitude,
+							selectedLocation.longitude
+						),
+						level: 3,
+					};
+					const map = new window.kakao.maps.Map(container, options);
+
+					// 마커 추가
+					const marker = new window.kakao.maps.Marker({
+						map: map,
+						position: new window.kakao.maps.LatLng(
+							selectedLocation.latitude,
+							selectedLocation.longitude
+						),
+					});
+
+					// 지도 리사이즈 및 중심 재조정
+					window.kakao.maps.event.addListener(map, "tilesloaded", () => {
+						map.relayout();
+						map.setCenter(
+							new window.kakao.maps.LatLng(
+								selectedLocation.latitude,
+								selectedLocation.longitude
+							)
+						);
+					});
 				});
 			};
 		};
 
-		const initializeMap = () => {
-			console.log("Kakao Maps is ready");
-			if (!selectedLocation) return;
-
-			const container = document.getElementById("map");
-			const options = {
-				center: new window.kakao.maps.LatLng(
-					selectedLocation.latitude,
-					selectedLocation.longitude
-				),
-				level: 3,
-			};
-			const map = new window.kakao.maps.Map(container, options);
-
-			// 마커 추가
-			new window.kakao.maps.Marker({
-				map: map,
-				position: new window.kakao.maps.LatLng(
-					selectedLocation.latitude,
-					selectedLocation.longitude
-				),
-			});
-
-			// 지도 리사이즈 및 중심 재조정
-			window.kakao.maps.event.addListener(map, "tilesloaded", () => {
-				map.relayout();
-				map.setCenter(
-					new window.kakao.maps.LatLng(
-						selectedLocation.latitude,
-						selectedLocation.longitude
-					)
-				);
-			});
-		};
-
 		loadScript();
 
-		// 컴포넌트 언마운트 시 스크립트 제거
+		// 컴포넌트 언마운트 시 스크립트와 지도 객체 정리
 		return () => {
 			const kakaoScript = document.querySelector("script[src*='kakao']");
 			if (kakaoScript) {
 				document.head.removeChild(kakaoScript);
+			}
+			const container = document.getElementById("map");
+			if (container) {
+				while (container.firstChild) {
+					container.removeChild(container.firstChild);
+				}
 			}
 		};
 	}, [selectedLocation]);
