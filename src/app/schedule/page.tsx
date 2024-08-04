63148759835b166bfd1131c549aa7ae457cad2fd
superrookie8 <74133113+superrookie8@users.noreply.github.com>
2024-08-05 04:23:20 +0900

    direction info

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -6,6 +6,7 @@ import { locations } from "@/data/schedule";
 import Map from "@/components/kakaoMap";
 import { useSetRecoilState } from "recoil";
 import { selectedLocationState } from "@/states/locationState";
+import DirectionsGuide from "@/components/DirectionsGuide";
 
 const Schedule: React.FC = () => {
 	const setSelectedLocation = useSetRecoilState(selectedLocationState);
@@ -17,13 +18,20 @@ const Schedule: React.FC = () => {
 
 	return (
 		<div className="flex overflow-x-hidden justify-center items-center min-h-screen w-full">
-			<div className="flex flex-col  justify-center p-4 w-full max-w-screen-lg">
+			<div className="flex flex-col justify-center p-4 w-full max-w-screen-lg">
 				<div className="flex flex-col  md:flex-row lg:flex-row  md:space-x-4 lg:space-x-4">
 					<div className="bg-white w-full md:w-1/2 lg:w-1/2 mb-4 md:mb-0 lg:mb-0 rounded-lg shadow-lg">
 						<Calendar />
 					</div>
-					<div className="bg-white relative w-full md:w-1/2 lg:w-1/2 h-[400px] md:h-auto lg:h-auto p-4 rounded-lg shadow-lg">
-						<Map />
+
+					<div className="bg-white relative w-full md:w-1/2 lg:w-1/2  md:h-auto lg:h-auto p-4 rounded-lg shadow-lg">
+						<div className="lg:h-[400px] md: h-[400px] sm: h-[200px]">
+							<Map />
+						</div>
+						<div className="mt-4 p-4 h-[250px] sm:h-auto border border-red-500 rounded">
+							{" "}
+							<DirectionsGuide />
+						</div>
 					</div>
 				</div>
 			</div>
