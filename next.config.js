9c749548f44e112955c23bb9b2e939edafcbad41
superrookie8 <74133113+superrookie8@users.noreply.github.com>
2024-08-05 22:48:11 +0900

    guestbook read test

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ const nextConfig = {
 				headers: [
 					{
 						key: "Cache-Control",
-						value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+						value:
+							"no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
 					},
 				],
 			},
