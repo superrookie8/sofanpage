0b3de760dcaf72ad4309a8521ae1b9739e291f62
superrookie8 <74133113+superrookie8@users.noreply.github.com>
2024-09-11 01:12:08 +0900

    벽돌깨기 게임 작동 수정, 모바일 배경고정 설정

--- src/components/brickbreakergame.js
+++ src/components/brickbreakergame.js
@@ -35,9 +35,9 @@ const BrickBreakerGame = ({ onGameOver, currentGame, difficulty }) => {
 		let y = canvas.height - 30;
 		let dx = 2;
 		let dy = -2;
-		const ballRadius = 8; // Smaller ball radius
-		const paddleHeight = 8; // Smaller paddle height
-		const paddleWidth = difficulty === "easy" ? 150 : 50; // Longer paddle for easy mode
+		const ballRadius = 8;
+		const paddleHeight = 8;
+		const paddleWidth = difficulty === "easy" ? 150 : 50;
 		let paddleX = (canvas.width - paddleWidth) / 2;
 
 		const brickRowCount = 15;
@@ -56,26 +56,14 @@ const BrickBreakerGame = ({ onGameOver, currentGame, difficulty }) => {
 			}
 		}
 
-		let rightPressed = false;
-		let leftPressed = false;
 		let bricksBroken = 0;
 
-		document.addEventListener("keydown", keyDownHandler, false);
-		document.addEventListener("keyup", keyUpHandler, false);
+		document.addEventListener("mousemove", mouseMoveHandler, false);
 
-		function keyDownHandler(e) {
-			if (e.key === "Right" || e.key === "ArrowRight") {
-				rightPressed = true;
-			} else if (e.key === "Left" || e.key === "ArrowLeft") {
-				leftPressed = true;
-			}
-		}
-
-		function keyUpHandler(e) {
-			if (e.key === "Right" || e.key === "ArrowRight") {
-				rightPressed = false;
-			} else if (e.key === "Left" || e.key === "ArrowLeft") {
-				leftPressed = false;
+		function mouseMoveHandler(e) {
+			const relativeX = e.clientX - canvas.offsetLeft;
+			if (relativeX > 0 && relativeX < canvas.width) {
+				paddleX = relativeX - paddleWidth / 2;
 			}
 		}
 
@@ -105,7 +93,7 @@ const BrickBreakerGame = ({ onGameOver, currentGame, difficulty }) => {
 		function drawBall() {
 			ctx.beginPath();
 			ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-			ctx.fillStyle = "#FF0000"; // Red color
+			ctx.fillStyle = "#FF0000";
 			ctx.fill();
 			ctx.closePath();
 		}
@@ -118,7 +106,7 @@ const BrickBreakerGame = ({ onGameOver, currentGame, difficulty }) => {
 				paddleWidth,
 				paddleHeight
 			);
-			ctx.fillStyle = "#FF0000"; // Red color
+			ctx.fillStyle = "#FF0000";
 			ctx.fill();
 			ctx.closePath();
 		}
@@ -175,12 +163,6 @@ const BrickBreakerGame = ({ onGameOver, currentGame, difficulty }) => {
 				}
 			}
 
-			if (rightPressed && paddleX < canvas.width - paddleWidth) {
-				paddleX += 7;
-			} else if (leftPressed && paddleX > 0) {
-				paddleX -= 7;
-			}
-
 			x += dx;
 			y += dy;
 			requestAnimationFrame(draw);
@@ -196,8 +178,7 @@ const BrickBreakerGame = ({ onGameOver, currentGame, difficulty }) => {
 		};
 
 		return () => {
-			document.removeEventListener("keydown", keyDownHandler);
-			document.removeEventListener("keyup", keyUpHandler);
+			document.removeEventListener("mousemove", mouseMoveHandler);
 		};
 	}, [onGameOver, currentGame, difficulty, gameInitialized]);
 
