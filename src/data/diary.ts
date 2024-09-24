d63260c72a7f89330004e6f91bd0aa88d3c7e927
superrookie8 <74133113+superrookie8@users.noreply.github.com>
2024-09-24 17:19:07 +0900

    중복코드 삭제

--- src/data/diary.ts
+++ src/data/diary.ts
@@ -19,20 +19,4 @@ export interface DiaryEntry {
 		number: string;
 	};
 }
-// export interface DiaryEntry {
-// 	_id: string;
-// 	nickname: string;
-// 	name: string;
-// 	date: string;
-// 	weather: string;
-// 	location: string;
-// 	together: string;
-// 	win_status: string;
-// 	diary_photo?:string;
-// 	diary_message: string;
-// 	seat_info: {
-// 		section: string;
-// 		row: string;
-// 		number: string;
-// 	};
-// }
+
